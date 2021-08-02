7b42d3ee50fbfdc496c80817b17153aa594298f4
Antonia Sacchitella <Antonia.Sacchitella@icubed.it>
2021-08-02 15:32:15 +0200

    esercitazione n.1

---
+++ ExceptionWeek6.Demo/Week6-Esercitazione1PP/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.IO;
+
+namespace Week6_Esercitazione1PP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ProgrammazionePreventiva();
+            ProgrammazioneConEccezioni();
+        }
+
+        static void ProgrammazioneConEccezioni()
+        {
+            int[] arrayInteri = new int[5];
+            string path = @"C:\Users\AntoniaSacchitella\Desktop\Week6-PreAcademyG\provaFile.txt";
+            StreamWriter sw = null;
+            try
+            {
+                Console.WriteLine("Inserisci il numero da aggiungere");
+                int value = int.Parse(Console.ReadLine());
+                Console.WriteLine("Inserisci posizione da popolare");
+                int index = int.Parse(Console.ReadLine());
+                arrayInteri[index] = value;
+                sw = new StreamWriter(path);
+                for(int i = 0; i < arrayInteri.Length; i++)
+                {
+                    sw.WriteLine(arrayInteri[i]);
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File non trovato");
+            }
+            catch (FileLoadException)
+            {
+                Console.WriteLine("Errore di caricamento");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Puoi inserire solo numeri");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine($"Puoi inserire solo numeri tra {0} e {1}", int.MinValue, int.MaxValue);
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("La posizione inserita non è valida");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if(sw != null)
+                    sw.Close();
+            }
+        }
+
+        static void ProgrammazionePreventiva()
+        {
+            //Dichiariamo un array di 5 elementi
+            int[] arrayInteri = new int[5];
+            string path = @"C:\Users\AntoniaSacchitella\Desktop\Week6-PreAcademyG\provaFile.txt";
+            Console.WriteLine("Inserisci il numero da aggiungere");
+            //INPUT UTENTE
+            bool successValue = int.TryParse(Console.ReadLine(), out int value);
+            Console.WriteLine("Inserisci posizione da occupare");
+            //INPUT UTENTE
+            bool successIndex = int.TryParse(Console.ReadLine(), out int index);
+            //if(successValue == true && successIndex == true)
+            if (successValue && successIndex)
+            {
+                if (index > 0 && index < arrayInteri.Length)
+                {
+                    arrayInteri[index] = value;
+                    if (File.Exists(path))
+                    {
+                        using (StreamWriter sw = new StreamWriter(path))
+                        {
+                            for (int i = 0; i < arrayInteri.Length; i++)
+                            {
+                                sw.WriteLine(arrayInteri[i]);
+                            }
+                        }
+
+
+                        //sw.Close();
+                    }
+                }
+            }
+        }
+    }
+}
